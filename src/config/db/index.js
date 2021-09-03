6c6b30348cade2e00b89acd39fa4c10bbf5666ef
Teacher chung <vuchung682001@gmail.com>
2021-09-03 11:00:28 +0700

    test

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/demo1', {
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/demo1', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
