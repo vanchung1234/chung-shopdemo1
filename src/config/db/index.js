41c381b1ace62fda12430f90cfee85b7b87814c4
Teacher chung <vuchung682001@gmail.com>
2021-09-03 12:50:16 +0700

    test

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://chungdev:vanchung123@cluster0.pkavz.mongodb.net/demo1?retryWrites=true&w=majority', {
+mongoose.connect(process.env.MONGODB_URL || 'mongodb+srv://chungdev:vanchung123@cluster0.pkavz.mongodb.net/demo1?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
