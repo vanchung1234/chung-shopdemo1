7f349cd8307263125de9c8197ccac64893641f53
Teacher chung <vuchung682001@gmail.com>
2021-09-06 20:34:10 +0700

    test

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,4 +1,4 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.DATABASE_URL || 'mongodb+srv://chungdev:vanchung123@cluster0.pkavz.mongodb.net/demo1?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
 module.exports = mongoose;
